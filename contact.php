8d54c6ab99d9004c0164fc3d5743413651822277
josueleto77 <94468870+josueleto77@users.noreply.github.com>
2022-05-30 19:15:11 -0400

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,33 +1,29 @@
 <?php 
 
  //contact form info
- $Firstname=  $_POST['Firstname'];
- $Lastname=  $_POST['Lastname'];
+ $name=  $_POST['name'];
  $email=  $_POST['email'];
  $subject = $_POST['subject'];
  $contact_message = $_POST['message'];
 
 
  $message = "<b>Mail Sender Info:</b> </br>
-            <h5><b>Name:</b>".$Firstname." ".$Lastname."</h5>
+            <h5><b>Name:</b>".$name."</h5>
             <h5><b>Email:</b>".$email."</h5>
             </br>
             <p>".$contact_message."</p>";
             
             
- $to = "info@nxispwr.com". "r/n";
+ $to = "info@nxispwr.com"; //Replace your real receiving email address
 
- $header = "From: info@nxispwr.com ". "\r\n";
- $header .= "X-Mailer: PHP/". phpversion();
- $header .= "Content-type: text/html". "r/n";
+ $header = "From:info@nxispwr.com \r\n"; //Replace with your real web master email
+ $header .= "MIME-Version: 1.0\r\n";
+ $header .= "Content-type: text/html\r\n";
  
- $mail = @mail($to,$subject,$message,$header);
+ $mail_send = mail ($to,$subject,$message,$header);
  
  if( $mail_send == true ) {
-    echo "<h4>Your message send successfully!.</h4>";
+    echo "Your message send successfully!.";
  }else {
-    echo "<h4>Your message could not be send!.</h4>";
+    echo "Your message could not be send!.";
  }
-
-
-?>
