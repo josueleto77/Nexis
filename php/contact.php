7023aa3f2a03417d8696c0532535027816f0f1a0
josueleto77 <94468870+josueleto77@users.noreply.github.com>
2022-05-30 19:50:23 -0400

    Add files via upload

---
+++ php/contact.php
@@ -0,0 +1,33 @@
+<?php 
+
+ //contact form info
+ $Firstname=  $_POST['Firstname'];
+ $Lastname=  $_POST['Lastname'];
+ $email=  $_POST['email'];
+ $subject = $_POST['subject'];
+ $contact_message = $_POST['message'];
+
+
+ $message = "<b>Mail Sender Info:</b> </br>
+            <h5><b>Name:</b>".$Firstname." ".$Lastname."</h5>
+            <h5><b>Email:</b>".$email."</h5>
+            </br>
+            <p>".$contact_message."</p>";
+            
+            
+ $to = "leads@nxispwr.com"; //Replace your real receiving email address
+
+ $header = "From:info@nxispwr.com \r\n"; //Replace with your real web master email
+ $header .= "MIME-Version: 1.0\r\n";
+ $header .= "Content-type: text/html\r\n";
+ 
+ $mail_send = mail ($to,$subject,$message,$header);
+ 
+ if( $mail_send == true ) {
+    echo "Your message send successfully!.";
+ }else {
+    echo "Your message could not be send!.";
+ }
+
+
+?>
